eb356e69743d1207f88a68fdd59c3c1bfbfdd025
Sunderhus <matheus.sunderhus@gmail.com>
2022-06-30 11:34:59 -0300

    chore: move main index react file to composition root folder

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '@/presentation/App'
 import '@/main/config/styles/global.css'
+import { Event } from '@/presentation/pages'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Event />
   </React.StrictMode>
 )
--- src/presentation/App.tsx
+++
@@ -1,8 +0,0 @@
-const App:React.FC = ()=> {
-
-  return (
-   <h1 className="text-2xl font-bold">H e  ll o</h1>
-  )
-}
-
-export default App
